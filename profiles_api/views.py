3999c11beca5be5c5f39688e19b7a7016b3e7fce
Hendi Rusfandi <socialmedia.rusfandi@gmail.com>
2019-10-08 11:54:15 +0700

    change user word to users

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -125,5 +125,5 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
 
     def perform_create(self, serializer):
-        """Sets the user profile to the logged in user"""
+        """Sets the user profile to the logged in users"""
         serializer.save(user_profile=self.request.user)
